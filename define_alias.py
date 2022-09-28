a2137aa1a83c9b2a5b64fe1f0370dbcd4e965d0c
Niels Warncke <niels.warncke@gmail.com>
2022-09-28 10:17:53 +0200

    fix alias

--- define_alias.py
+++ define_alias.py
@@ -2,7 +2,8 @@ import os
 
 # Start app
 dirname = os.path.dirname(__file__)
-cmd = f'\\"python3 {dirname}/eliza_effect.py\\"'
+cmd = f'\\"python3 \\\\\\\"{dirname}/eliza_effect.py\\\\\\\" \\"'
 alias = f"alias eliza='{cmd}'"
+print(alias)
 os.system(f"echo '\n{alias}' >> ~/.bashrc")
 os.system(f"echo '\n{alias}' >> ~/.zshrc")
