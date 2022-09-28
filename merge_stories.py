ff5e5ad03e59d0497fbb55ef8fc9b0de6c61a570
Niels Warncke <niels.warncke@gmail.com>
2022-09-28 13:48:29 +0200

    regenerate after merge

--- merge_stories.py
+++ merge_stories.py
@@ -14,6 +14,11 @@ for story_path in stories:
         story[0]["color"] = "blue"
         merged += story
 
+for part in merged:
+    if part["media"] != "extern":
+        part["src"] = ""
+
+
 import os
 os.makedirs("data//The-ELIZA-Effect", exist_ok=True)
 with open(f"data/The-ELIZA-Effect/medias.json", "w") as f:
