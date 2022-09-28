d59399d027fa5efb7db1286df64287b8482a41b1
Niels Warncke <niels.warncke@gmail.com>
2022-09-28 13:30:58 +0200

    merge stories

---
+++ merge_stories.py
@@ -0,0 +1,20 @@
+import json
+
+
+stories = [
+    "short",
+    "tutorial"
+]
+
+merged = []
+for story_path in stories:
+    with open(f"data/{story_path}/generated/medias.json", "r") as f:
+        story = json.load(f)
+        story[0]["title"] = story_path
+        story[0]["color"] = "blue"
+        merged += story
+
+import os
+os.makedirs("data//The-ELIZA-Effect", exist_ok=True)
+with open(f"data/The-ELIZA-Effect/medias.json", "w") as f:
+    json.dump(merged, f)
