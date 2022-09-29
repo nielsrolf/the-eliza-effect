a4684162d006b5b7746e89f7e6c2fb83be677de5
ton <ton@KH3-MacMini.local>
2022-09-29 13:43:29 +0200

    fixes

--- merge_stories.py
+++ merge_stories.py
@@ -4,10 +4,10 @@ import json
 stories = [
     "Beginn",
     "Beginn2",
-    "1KI",
+    "1KI/2.KI.txt",
     "ZS1",
     "2Nein",
-    "3Marin",
+    "3Marin/3Marin",
     "4Was-ist",
     "5The-Lila",
     "5The-Lila2",
@@ -20,12 +20,19 @@ stories = [
     "ZS3"]
 
 merged = []
+import os
 for story_path in stories:
-    with open(f"data/{story_path}/generated/medias.json", "r") as f:
+    path = f"data/{story_path}/generated/medias.json"
+    if not os.path.exists(path):
+        path = f"data/{story_path}/medias.json"
+    with open(path, "r") as f:
         story = json.load(f)
         story[0]["title"] = story_path
         story[0]["color"] = "blue"
         merged += story
+for part in merged:
+    if part["media"] != "extern":
+        part["src"] = None
 
 import os
 os.makedirs("data//The-ELIZA-Effect", exist_ok=True)
